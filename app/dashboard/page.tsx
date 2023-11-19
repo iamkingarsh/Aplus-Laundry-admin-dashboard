528fa4c4f4f5a6ef9bd5c3cb8a2ca55e8fb306bd
Mohammed Arshad <contact@mohammedarshad.com>
2023-11-20 02:24:25 +0530

    Added Stat Cards

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,14 +1,53 @@
+import StatsCard from '@/components/statscard'
 import { Button } from '@/components/ui/button'
 import Heading from '@/components/ui/heading'
+import { DollarSignIcon, IndianRupeeIcon, LucidePlusCircle, PlusIcon, ShoppingBagIcon, Users } from 'lucide-react'
 
 
 export default function page() {
+    const StatsData = [
+        {
+            title: 'Total Revenue',
+            stat: 2300,
+            statPrefix: '$',
+            icon: <IndianRupeeIcon />,
+            desc: '+180.1% from last month'
+        },
+        {
+            title: 'Total Orders',
+            stat: 30,
+            statPrefix: '+',
+            icon: <ShoppingBagIcon />,
+            desc: '+180.1% from last month'
+        },
+        {
+            title: 'Total Subscribers',
+            stat: 350,
+            statPrefix: '+',
+            icon: <Users />,
+            desc: '+180.1% from last month'
+        },
+        {
+            title: 'Total Subscriber Orders',
+            stat: 230,
+            statPrefix: '+',
+            icon: <ShoppingBagIcon />,
+            desc: '+180.1% from last month'
+        },
+    ]
 
     return (
-        <div className='w-full h-full flex p-6 flex-row'>
+        <div className='w-full space-y-4 h-full flex p-6 flex-col'>
             <div className="topbar w-full flex justify-between">
                 <Heading title='Dashboard' />
-                <Button variant='default'>Create New Order</Button>
+                <Button variant='default'>Create New <PlusIcon className='w-4 ml-2' /></Button>
+            </div>
+            <div className='w-full flex gap-2'>
+                {StatsData.map((data, index) => {
+                    return (
+                        <StatsCard key={index} title={data.title} statPrefix={data.statPrefix} stat={data.stat} icon={data.icon} desc={data.desc} />
+                    )
+                })}
             </div>
         </div>
     )
---
+++ components/statscard.tsx
@@ -0,0 +1,43 @@
+
+import React from 'react';
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from './ui/card';
+import { Button } from './ui/button';
+import Heading from './ui/heading';
+import { ArrowBigRightDashIcon, ArrowRight, ArrowRightFromLineIcon, ArrowRightIcon, LucideArrowUpRight } from 'lucide-react';
+
+
+interface StatsCardProps {
+    title: string;
+    stat: number;
+    statPrefix?: string;
+    icon: React.ReactNode;
+    desc?: string;
+    clasname?: string;
+}
+
+const StatsCard: React.FC<StatsCardProps> = ({
+    title,
+    stat,
+    statPrefix = '',
+    icon,
+    desc,
+    clasname
+}) => {
+    return (
+        <Card className='text-left w-full flex flex-col justify-evenly items-stretch relative'>
+            <CardHeader>
+                <CardTitle className='text-md font-normal'>{title}</CardTitle>
+                <Heading title={`${statPrefix}${stat}`} className='text-3xl font-extrabold' />
+                <p className='opacity-80 text-sm py-1'>{desc}</p>
+                <Button variant='secondary'>View All <ArrowRight className='w-4 ml-3' /></Button>
+            </CardHeader>
+
+
+            <div className='absolute top-5 right-5'>
+                {icon}
+            </div>
+        </Card>
+    );
+};
+
+export default StatsCard;
