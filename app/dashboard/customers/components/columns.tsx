ba01b089d0fb8f6fb85ef481993d052cc104fffd
Mohammed Arshad <contact@mohammedarshad.com>
2023-11-21 20:23:23 +0530

    Added Action Menu for Data Table

--- app/dashboard/customers/components/columns.tsx
+++ app/dashboard/customers/components/columns.tsx
@@ -2,9 +2,18 @@
 
 import { Button } from "@/components/ui/button"
 import { ColumnDef } from "@tanstack/react-table"
-import { ArrowUpDown, MoreHorizontal } from "lucide-react"
+import { ArrowUpDown, Delete, Edit, Edit2, MoreHorizontal, ToggleLeft, Trash, User } from "lucide-react"
 import { Checkbox } from "@/components/ui/checkbox"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { Switch } from "@/components/ui/switch"
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
 export type CustomersColumns = {
@@ -115,6 +124,36 @@ export const columns: ColumnDef<CustomersColumns>[] = [
                 {row.original.status}
             </div>
         ),
+    },
+    {
+        id: "actions",
+        cell: ({ row }) => {
+            const payment = row.original
+
+            return (
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <Button variant="ghost" className="h-8 w-8 p-0">
+                            <span className="sr-only">Open menu</span>
+                            <MoreHorizontal className="h-4 w-4" />
+                        </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent className="gap-2" align="end">
+                        <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem>
+                            <User className="mr-2 h-4 w-4" />
+                            View Customer Details</DropdownMenuItem>
+                        <DropdownMenuItem>
+                            <Edit2 className="mr-2 h-4 w-4" />
+                            Edit Customer Details</DropdownMenuItem>
+                        <DropdownMenuItem className="focus:bg-destructive focus:text-destructive-foreground">
+                            <Trash className="mr-2 h-4 w-4" />
+                            Delete Customer</DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+            )
+        },
     },
 
 ]
