0754cdce55a075df84990b8c8078570c0462dd06
Mohammed Arshad <contact@mohammedarshad.com>
2023-11-21 20:03:16 +0530

    Added Visibility to data table

--- components/ui/data-table.tsx
+++ components/ui/data-table.tsx
@@ -4,12 +4,14 @@ import {
     ColumnDef,
     ColumnFiltersState,
     SortingState,
+    VisibilityState,
     flexRender,
     getCoreRowModel,
     getFilteredRowModel,
     getSortedRowModel,
     useReactTable,
     getPaginationRowModel,
+
 } from "@tanstack/react-table"
 
 import {
@@ -23,6 +25,12 @@ import {
 import { Button } from "./button"
 import { Input } from "@/components/ui/input"
 import { Search } from "lucide-react"
+import {
+    DropdownMenu,
+    DropdownMenuCheckboxItem,
+    DropdownMenuContent,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
 
 interface DataTableProps<TData, TValue> {
     columns: ColumnDef<TData, TValue>[]
@@ -40,6 +48,7 @@ export function DataTable<TData, TValue>({
     const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
         []
     )
+    const [columnVisibility, setColumnVisibility] = React.useState<VisibilityState>({})
     const table = useReactTable({
         data,
         columns,
@@ -50,10 +59,12 @@ export function DataTable<TData, TValue>({
         onColumnFiltersChange: setColumnFilters,
         getFilteredRowModel: getFilteredRowModel(),
         onRowSelectionChange: setRowSelection,
+        onColumnVisibilityChange: setColumnVisibility,
         state: {
             sorting,
             columnFilters,
             rowSelection,
+            columnVisibility,
         },
     })
 
@@ -71,6 +82,34 @@ export function DataTable<TData, TValue>({
                         className="max-w-sm"
                     />
                 </div>
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <Button variant="outline" className="ml-auto">
+                            Columns
+                        </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent align="end">
+                        {table
+                            .getAllColumns()
+                            .filter(
+                                (column) => column.getCanHide()
+                            )
+                            .map((column) => {
+                                return (
+                                    <DropdownMenuCheckboxItem
+                                        key={column.id}
+                                        className="capitalize"
+                                        checked={column.getIsVisible()}
+                                        onCheckedChange={(value) =>
+                                            column.toggleVisibility(!!value)
+                                        }
+                                    >
+                                        {column.id}
+                                    </DropdownMenuCheckboxItem>
+                                )
+                            })}
+                    </DropdownMenuContent>
+                </DropdownMenu>
             </div>
 
             <div className="rounded-md border">
@@ -110,7 +149,7 @@ export function DataTable<TData, TValue>({
                         ) : (
                             <TableRow>
                                 <TableCell colSpan={columns.length} className="h-24 text-center">
-                                    No results.
+                                    No results Found.
                                 </TableCell>
                             </TableRow>
                         )}
@@ -120,7 +159,7 @@ export function DataTable<TData, TValue>({
             <div className="flex items-center justify-between space-x-2 py-4">
                 <div>
 
-                    {table.getFilteredSelectedRowModel().rows.length > 0 && <span className="ml-2  text-sm font-semibold">
+                    {table.getFilteredSelectedRowModel().rows.length > 0 && <span className="ml-2  text-sm font-semibold text-muted-foreground">
                         {table.getFilteredSelectedRowModel().rows.length} of{" "}
                         {table.getFilteredRowModel().rows.length} row(s) selected.
                     </span>}
