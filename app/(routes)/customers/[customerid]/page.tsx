1d7e8ad62bcc120d0e58b047b632829535919834
Mohammed Arshad <mohammedarshad.arsh@gmail.com>
2023-12-08 22:58:50 +0530

    Refactor customer page component and add personal information card

--- app/(routes)/customers/[customerid]/page.tsx
+++ app/(routes)/customers/[customerid]/page.tsx
@@ -1,14 +1,17 @@
-import { NewCustomerForm } from '@/components/forms/newCustomerForm';
 import Heading from '@/components/ui/heading';
-import { Icons } from '@/components/ui/icons';
 import { Separator } from '@/components/ui/separator';
-import { useRouter } from 'next/navigation';
+import { AllData } from '../page';
+import { Mail, Phone, User } from 'lucide-react';
+import { Card, CardContent, CardHeader } from '@/components/ui/card';
 
 interface Props {
     params: {
         customerid: string;
     }
+
 }
+
+const customerData = AllData[0];
 
 
 export default function CustomerPage({ params }: Props) {
@@ -16,14 +19,49 @@ export default function CustomerPage({ params }: Props) {
         <div className='w-full space-y-2 h-full flex p-6 flex-col'>
             <div className="topbar w-full flex justify-between">
                 <div>
-                    <Heading className='leading-tight' title='Customer Details - ' />
-                    <p className='text-muted-foreground text-sm'>Fill in the form below to create a new customer</p>
+                    <Heading className='leading-tight' title={`Customer Details - ${customerData.fullname}`} />
+                    <p className='text-muted-foreground text-sm'>Customer ID: {params.customerid}</p>
                 </div>
 
             </div>
             <Separator orientation='horizontal' />
-            <div className="container mx-auto py-10">
+            <div className="container mx-auto grid  grid-cols-2 py-10">
+                <Card >
+                    <CardHeader>
+                        <Heading className='text-lg' title='Personal Information' />
+                    </CardHeader>
+                    <Separator className='mb-2' orientation='horizontal' />
+                    <CardContent>
 
+
+                        <div className="flex flex-col gap-3">
+                            <div className='flex items-center'>
+                                <User className='w-6 h-6 mr-2' />
+                                <div className="flex flex-col">
+                                    <span className="text-muted-foreground  text-sm">Name</span>
+                                    <span className="text-md">{customerData.fullname}</span>
+                                </div>
+                            </div>
+                            <div className='flex items-center'>
+                                <Mail className='w-6 h-6 mr-2' />
+                                <div className="flex flex-col">
+                                    <span className="text-muted-foreground  text-sm">Email</span>
+                                    <span className="text-md">{customerData.email}</span>
+                                </div>
+                            </div>
+                            <div className='flex items-center'>
+                                <Phone className='w-6 h-6 mr-2' />
+                                <div className="flex flex-col">
+                                    <span className=" text-muted-foreground  text-sm">Mobile</span>
+                                    <span className="text-md">{customerData.mobile}</span>
+                                </div>
+                            </div>
+                        </div>
+
+
+                    </CardContent>
+
+                </Card>
             </div>
 
 
