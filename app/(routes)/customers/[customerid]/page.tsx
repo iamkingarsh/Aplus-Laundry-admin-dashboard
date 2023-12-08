4e9b404974196b10272e921ff8b4bf71034f2eb0
Mohammed Arshad <mohammedarshad.arsh@gmail.com>
2023-12-08 23:43:18 +0530

    Update customer page with additional details and live orders

--- app/(routes)/customers/[customerid]/page.tsx
+++ app/(routes)/customers/[customerid]/page.tsx
@@ -1,8 +1,11 @@
 import Heading from '@/components/ui/heading';
 import { Separator } from '@/components/ui/separator';
 import { AllData } from '../page';
-import { Mail, Phone, User } from 'lucide-react';
+import { Mail, MapPin, Phone, Pin, User } from 'lucide-react';
 import { Card, CardContent, CardHeader } from '@/components/ui/card';
+import Link from 'next/link';
+import { Badge } from '@/components/ui/badge';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 
 interface Props {
     params: {
@@ -11,7 +14,16 @@ interface Props {
 
 }
 
-const customerData = AllData[0];
+
+const customerDetails = [
+
+    AllData[0].emailVerified = true,
+    AllData[0].mobileVerified = true,
+    AllData[0].city = 'Hyderabad',
+    AllData[0].pincode = '500050',
+    AllData[0].state = 'Telangana',
+]
+const customerData = { ...AllData[0], ...customerDetails };
 
 
 export default function CustomerPage({ params }: Props) {
@@ -25,7 +37,7 @@ export default function CustomerPage({ params }: Props) {
 
             </div>
             <Separator orientation='horizontal' />
-            <div className="container mx-auto grid  grid-cols-2 py-10">
+            <div className="container mx-auto grid  grid-cols-2 gap-4 py-10">
                 <Card >
                     <CardHeader>
                         <Heading className='text-lg' title='Personal Information' />
@@ -35,25 +47,70 @@ export default function CustomerPage({ params }: Props) {
 
 
                         <div className="flex flex-col gap-3">
-                            <div className='flex items-center'>
-                                <User className='w-6 h-6 mr-2' />
-                                <div className="flex flex-col">
-                                    <span className="text-muted-foreground  text-sm">Name</span>
-                                    <span className="text-md">{customerData.fullname}</span>
+                            <div className='flex justify-between items-center'>
+                                <div className='flex items-center'>
+                                    <User className='w-6 h-6 mr-3' />
+                                    <div className="flex flex-col">
+                                        <span className="text-muted-foreground  text-sm">Name</span>
+                                        <span className="text-md">{customerData.fullname}</span>
+                                    </div>
                                 </div>
+                                <Avatar className='w-8  border-muted border-2 h-8 mr-2'>
+                                    <AvatarImage src={customerData.profilepic} alt="@shadcn" />
+                                    <AvatarFallback>{customerData.fullname[0]}</AvatarFallback>
+                                </Avatar>
                             </div>
+                            <Separator orientation='horizontal' />
+
                             <div className='flex items-center'>
-                                <Mail className='w-6 h-6 mr-2' />
+                                <Mail className='w-6 h-6 mr-3' />
                                 <div className="flex flex-col">
                                     <span className="text-muted-foreground  text-sm">Email</span>
-                                    <span className="text-md">{customerData.email}</span>
+                                    <div className='flex gap-2'>
+
+                                        <Link href={`mailto:${customerData.email}`} className="text-md">{customerData.email}</Link> {customerData.emailVerified === true ? <Badge className='ml-2' variant="default" >Verified</Badge> : <Badge className='ml-2' variant="secondary" >Unverified</Badge>}
+                                    </div>
                                 </div>
                             </div>
+                            <Separator orientation='horizontal' />
+
                             <div className='flex items-center'>
-                                <Phone className='w-6 h-6 mr-2' />
+                                <Phone className='w-6 h-6 mr-3' />
                                 <div className="flex flex-col">
                                     <span className=" text-muted-foreground  text-sm">Mobile</span>
-                                    <span className="text-md">{customerData.mobile}</span>
+                                    <div className='flex gap-2'>
+
+                                        <Link href={`tel:${customerData.mobile}`} className="text-md">{customerData.mobile}</Link>
+                                        {customerData.mobile === true ? <Badge className='ml-2' variant="default" >Verified</Badge> : <Badge className='ml-2' variant="secondary"  >Unverified</Badge>}
+                                    </div>
+                                </div>
+                            </div>
+                            <Separator orientation='horizontal' />
+
+                            <div className='flex items-start'>
+                                <MapPin className='w-6 h-6 mr-3' />
+                                <div className='flex flex-col gap-2'>
+                                    <div className="flex flex-col">
+                                        <span className=" text-muted-foreground  text-sm">Address</span>
+                                        <Link href={``} className="text-md">{customerData.address}</Link>
+
+                                    </div>
+                                    <div className='flex gap-6'>
+
+                                        <div className="flex flex-col">
+                                            <span className=" text-muted-foreground  text-sm">City</span>
+                                            <Link href={``} className="text-md">{customerData.city}</Link>
+
+                                        </div>
+                                        <div className="flex flex-col">
+                                            <span className=" text-muted-foreground  text-sm">State</span>
+                                            <Link href={``} className="text-md">{customerData.state}</Link>
+                                        </div>
+                                        <div className="flex flex-col">
+                                            <span className=" text-muted-foreground  text-sm">Pincode</span>
+                                            <Link href={``} className="text-md">{customerData.pincode}</Link>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -62,6 +119,32 @@ export default function CustomerPage({ params }: Props) {
                     </CardContent>
 
                 </Card>
+                <div className='gap-4 flex flex-col'>
+                    <Card >
+                        <CardHeader>
+                            <Heading className='text-lg' title='Live Orders' />
+                        </CardHeader>
+                        <Separator className='mb-2' orientation='horizontal' />
+                        <CardContent>
+
+
+
+                        </CardContent>
+
+                    </Card>
+                    <Card >
+                        <CardHeader>
+                            <Heading className='text-lg' title='Live Orders' />
+                        </CardHeader>
+                        <Separator className='mb-2' orientation='horizontal' />
+                        <CardContent>
+
+
+
+                        </CardContent>
+
+                    </Card>
+                </div>
             </div>
 
 
