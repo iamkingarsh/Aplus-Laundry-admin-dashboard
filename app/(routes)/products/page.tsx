460e748c17091d334f8ae78d51de4008d0864f30
Mohammed Arshad <mohammedarshad.arsh@gmail.com>
2023-12-09 01:30:26 +0530

    Add LaundrtProducts array to page component

--- app/(routes)/products/page.tsx
+++ app/(routes)/products/page.tsx
@@ -1,7 +1,112 @@
 
 import Heading from '@/components/ui/heading'
 import { Separator } from '@/components/ui/separator'
+import { ServerIcon } from 'lucide-react'
 import React from 'react'
+
+const LaundrtProducts = [
+    {
+        title: 'Shirts',
+        value: 'shirts',
+        icon: <ServerIcon className="w-3 mr-2" />,
+        status: 'Active'
+    },
+    {
+        title: 'TShirts',
+        value: 'tshirts',
+        icon: <ServerIcon className="w-3 mr-2" />,
+        status: 'Active'
+    },
+    {
+        title: 'Trousers',
+        value: 'trousers',
+        icon: <ServerIcon className="w-3 mr-2" />,
+        status: 'Active'
+    },
+    {
+        title: 'Jeans',
+        value: 'jeans',
+        icon: <ServerIcon className="w-3 mr-2" />,
+        status: 'Active'
+    },
+    {
+        title: 'Shorts',
+        value: 'shorts',
+        icon: <ServerIcon className="w-3 mr-2" />,
+        status: 'Active'
+    },
+    {
+        title: 'Kurtas',
+        value: 'kurtas',
+        icon: <ServerIcon className="w-3 mr-2" />,
+        status: 'Active'
+    },
+    {
+        title: 'Kurtis',
+        value: 'kurtis',
+        icon: <ServerIcon className="w-3 mr-2" />,
+        status: 'Active'
+    },
+    {
+        title: 'Sarees',
+        value: 'sarees',
+        icon: <ServerIcon className="w-3 mr-2" />,
+        status: 'Active'
+    },
+    {
+        title: 'Bedsheets',
+        value: 'bedsheets',
+        icon: <ServerIcon className="w-3 mr-2" />,
+        status: 'Active'
+    },
+    {
+        title: 'Blankets',
+        value: 'blankets',
+        icon: <ServerIcon className="w-3 mr-2" />,
+        status: 'Active'
+    },
+    {
+        title: 'Curtains',
+        value: 'curtains',
+        icon: <ServerIcon className="w-3 mr-2" />,
+        status: 'Active'
+    },
+    {
+        title: 'CushionCovers',
+        value: 'cushioncovers',
+
+        icon: <ServerIcon className="w-3 mr-2" />,
+        status: 'Active'
+    },
+    {
+        title: 'PillowCovers',
+        value: 'pillowcovers',
+
+        icon: <ServerIcon className="w-3 mr-2" />,
+        status: 'Active'
+    },
+    {
+        title: 'Towels',
+        value: 'towels',
+
+        icon: <ServerIcon className="w-3 mr-2" />,
+        status: 'Active'
+    },
+    {
+        title: 'Masks',
+        value: 'masks',
+
+        icon: <ServerIcon className="w-3 mr-2" />,
+        status: 'Active'
+    },
+    {
+        title: 'Others',
+        value: 'others',
+
+        icon: <ServerIcon className="w-3 mr-2" />,
+        status: 'Active'
+    },
+]
 
 export default function page() {
     return (
@@ -13,6 +118,9 @@ export default function page() {
                 </div>
             </div>
             <Separator orientation='horizontal' />
+            <div className="container mx-auto py-10">
+
+            </div>
 
         </div>
     )
