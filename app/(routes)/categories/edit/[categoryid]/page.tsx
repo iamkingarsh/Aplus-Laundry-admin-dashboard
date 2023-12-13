92ea7ec053acdabf3b6b2d4ecae96c1d59294094
Mohammed Arshad <mohammedarshad.arsh@gmail.com>
2023-12-14 00:23:55 +0530

    Add Edit Cateogry Page

---
+++ app/(routes)/categories/edit/[categoryid]/page.tsx
@@ -0,0 +1,87 @@
+"use client"
+import { Alert } from '@/components/forms/Alert';
+import { EditCategoryForm } from '@/components/forms/editCategoryForm';
+import { EditCouponsForm } from '@/components/forms/editCouponForm';
+import { Button } from '@/components/ui/button';
+import Heading from '@/components/ui/heading';
+import { Icons } from '@/components/ui/icons';
+import { Separator } from '@/components/ui/separator';
+import { Switch } from '@/components/ui/switch';
+import { useGlobalModal } from '@/hooks/GlobalModal';
+import { categories } from '@/lib/constants';
+import { Group, Trash } from 'lucide-react';
+import { Metadata } from 'next';
+import { useRouter } from 'next/navigation';
+import React from 'react';
+import toast from 'react-hot-toast';
+
+
+
+interface Props {
+    params: {
+        categoryid: string;
+    }
+}
+
+
+
+
+export default function EditCategoryPage({ params }: Props) {
+    const categoryData = categories.filter((item: any) => item.category_id === params.categoryid)[0] as any
+
+    const useModal = useGlobalModal()
+    const router = useRouter()
+
+    const deleteCoupon = async (couponid: string) => {
+        // delete logic here
+        useModal.onClose()
+        toast.success('Category Deleted Successfully')
+        router.push('/categories')
+    }
+
+    return (
+        <div className='w-full space-y-2 h-full flex p-6 flex-col'>
+            <div className="topbar w-full flex items-center justify-between">
+                <div>
+                    <Heading className='leading-tight' title='Edit Category' />
+                    <p className='text-muted-foreground text-sm'>Edit your category details</p>
+                </div>
+                {categoryData &&
+                    <div className='flex items-center gap-3'>
+                        <Button
+                            onClick={() => {
+
+                                useModal.title = 'Are you sure you want to delete this category?'
+                                useModal.description = 'This action cannot be undone'
+                                useModal.children = <Alert onConfirm={() => deleteCoupon(params.categoryid)} />
+
+                                useModal.onOpen()
+                            }}
+                            variant='destructive'>
+                            <Trash className='w-4 ' />
+                        </Button>
+                    </div>}
+
+            </div>
+            <Separator orientation='horizontal' />
+            <div className="container mx-auto  py-10">
+
+                {categoryData ?
+                    <EditCategoryForm gap={3} categoryData={categoryData} />
+                    :
+                    <div className="flex flex-col gap-2 items-center justify-center h-full">
+
+                        <p className="text-muted-foreground text-lg">No Category Found</p>
+                        <Button onClick={() => router.push('/categories')} variant='default'>Go Back</Button>
+                    </div>
+                }
+            </div>
+
+
+        </div>
+
+    )
+}
+
+
+
---
+++ components/forms/editCategoryForm.tsx
@@ -0,0 +1,111 @@
+"use client"
+
+import * as React from "react"
+import * as z from "zod"
+
+import { cn } from "@/lib/utils"
+import { Icons } from "../ui/icons"
+import { Button } from "../ui/button"
+import { Input } from "../ui/input"
+import { Label } from "../ui/label"
+import { FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "../ui/form"
+import { useForm } from "react-hook-form"
+import { Form } from "../ui/form"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { CheckIcon, Plus } from "lucide-react"
+import toast from "react-hot-toast"
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover"
+import { CaretSortIcon } from "@radix-ui/react-icons"
+import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem } from "../ui/command"
+import { categories } from "@/lib/constants"
+
+
+interface EditCategoryFormProps extends React.HTMLAttributes<HTMLDivElement> {
+    gap: number
+    categoryData: any
+}
+
+const formSchema = z.object({
+    title: z.string().min(3, { message: "Category title must be atleast 3 characters long" }),
+
+
+
+})
+
+export function EditCategoryForm({ className, categoryData, gap, ...props }: EditCategoryFormProps) {
+    const [isLoading, setIsLoading] = React.useState<boolean>(false)
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            title: categoryData?.title || '',
+
+        },
+
+    })
+
+
+    function onSubmit(values: z.infer<typeof formSchema>) {
+        // Add submit logic here
+
+        setIsLoading(true)
+
+
+        setTimeout(() => {
+            setIsLoading(false)
+            toast.success('Customer created successfully')
+        }, 3000) // remove this timeout and add submit logic
+
+    }
+
+
+    return (
+        <div className={cn("grid gap-6 ", className)} {...props}>
+
+
+            <Form {...form} >
+
+                <form onSubmit={form.handleSubmit(onSubmit)} className=" grid grid-cols-1 gap-3">
+                    <div className={`grid grid-cols-${gap} gap-3`}>
+
+                        <FormField
+                            name="title"
+                            control={form.control}
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel htmlFor="title">Category Title</FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            id="title"
+                                            placeholder="eg.  Men"
+                                            type="Category Title"
+                                            autoCapitalize="none"
+                                            autoComplete="fullname"
+                                            autoCorrect="off"
+                                            disabled={isLoading}
+                                            {...field}
+                                        />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                    </div>
+                    <div className={`${gap === 2 ? 'w-full' : 'grid gap-3 grid-cols-3'}`} >
+                        <Button type="submit" className="w-full" disabled={isLoading}>
+                            {isLoading && (
+                                <Icons.spinner className="mr-2 h-4  w-4 animate-spin" />
+                            )}
+                            Update
+                        </Button>
+                    </div>
+
+                </form>
+            </Form>
+
+
+
+
+        </div>
+    )
+}
