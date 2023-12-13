9a593ef40cf495782bc0f966c3e08f8bd4faa929
Mohammed Arshad <mohammedarshad.arsh@gmail.com>
2023-12-13 23:35:30 +0530

    Add toast notification and router navigation to CellAction component

--- app/(routes)/products/components/cell-action.tsx
+++ app/(routes)/products/components/cell-action.tsx
@@ -6,6 +6,8 @@ import { Edit2, Eye, MoreHorizontal, Trash } from 'lucide-react'
 import React from 'react'
 import { ProductsColumns } from './columns'
 import { Alert } from '@/components/forms/Alert';
+import toast from 'react-hot-toast';
+import { useRouter } from 'next/navigation';
 
 interface Props {
     data: ProductsColumns
@@ -13,8 +15,10 @@ interface Props {
 
 export const CellAction: React.FC<Props> = ({ data }) => {
     const GlobalModal = useGlobalModal();
+    const router = useRouter()
     const deleteOrder = () => {
         console.log('delete')
+        toast.success('Item Deleted Successfully')
         GlobalModal.onClose()
     }
     return (
@@ -29,28 +33,27 @@ export const CellAction: React.FC<Props> = ({ data }) => {
                 <DropdownMenuContent className="gap-2" align="end">
                     <DropdownMenuLabel>Actions</DropdownMenuLabel>
                     <DropdownMenuSeparator />
-                    <DropdownMenuItem>
-                        <Eye className="mr-2 h-4 w-4" />
-                        View Customer Details</DropdownMenuItem>
-                    <DropdownMenuItem>
-                        <Eye className="mr-2 h-4 w-4" />
-                        View Order Details</DropdownMenuItem>
-                    <DropdownMenuItem>
-                        <Eye className="mr-2 h-4 w-4" />
-                        View Order Photos</DropdownMenuItem>
-                    <DropdownMenuItem>
+
+                    <DropdownMenuItem
+                        onSelect={
+                            () => {
+                                router.push(`/edit/${data.product_id}`)
+                            }
+                        }
+
+                    >
                         <Edit2 className="mr-2 h-4 w-4" />
-                        Edit Order Details</DropdownMenuItem>
+                        Edit Item</DropdownMenuItem>
                     <DropdownMenuItem
                         onSelect={() => {
-                            GlobalModal.title = "Delete Order"
-                            GlobalModal.description = "Are you sure you want to delete this order?"
+                            GlobalModal.title = `Delete Laundry Item`
+                            GlobalModal.description = "Are you sure you want to delete this laundry item? This action cannot be undone."
                             GlobalModal.children = <Alert onConfirm={deleteOrder} />
                             GlobalModal.onOpen()
                         }}
                         className="focus:bg-destructive focus:text-destructive-foreground">
                         <Trash className="mr-2 h-4 w-4" />
-                        Delete Order</DropdownMenuItem>
+                        Delete Item</DropdownMenuItem>
                 </DropdownMenuContent>
             </DropdownMenu>
         </div>
