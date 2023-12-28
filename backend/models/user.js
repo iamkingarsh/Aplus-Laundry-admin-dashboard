4d84d1dd1421d560047beca896a77591a16fedc8
Mohammed Arshad <mohammedarshad.arsh@gmail.com>
2023-12-28 13:38:40 +0530

    Update .env file and auth controller, fix user model, and modify index.js and page.tsx

--- backend/models/user.js
+++ backend/models/user.js
@@ -7,7 +7,8 @@ const userSchema = new mongoose.Schema({
     },
     email: {
         type: String,
-        unique: true
+        unique: true,
+        required:true,
     },
     mobileNumber: {
         type: Number, 
