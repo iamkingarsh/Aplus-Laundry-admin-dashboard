fb45f5e175a22eea3661ca56ff693049d5729c67
Mohd Mujahed <mujahedu984@gmail.com>
2024-01-01 19:42:20 +0530

    updated user

--- backend/models/user.js
+++ backend/models/user.js
@@ -11,7 +11,7 @@ const userSchema = new mongoose.Schema({
         required:true,
     },
     mobileNumber: {
-        type: Number, 
+        type: Number,  
         required: true,
         unique: true
     },
