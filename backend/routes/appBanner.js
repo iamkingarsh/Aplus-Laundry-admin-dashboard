8e0f2700868b74cb1ed28853e566609fc0f02ddf
Mohd Mujahed <mujahedu984@gmail.com>
2024-01-06 02:11:28 +0530

    updated  : appBanner

--- backend/routes/appBanner.js
+++ backend/routes/appBanner.js
@@ -1,19 +1,19 @@
 import express from 'express';  // Adjust the path based on your project structure
 import { createOrUpdateAppBanner, deleteAppBannerById, getAllAppBanners, getAppBannerById } from '../controllers/appBanner.js';
-import { authenticateToken } from '../middleware/authToken.js';
+import { adminAuthenticateToken } from '../middleware/authToken.js';
 
 const appBannerRouter = express.Router();
 
 // Create or update an app banner
-appBannerRouter.post('/adorupdate', authenticateToken, createOrUpdateAppBanner);
+appBannerRouter.post('/adorupdate', adminAuthenticateToken, createOrUpdateAppBanner);
 
 // Get all app banners
-appBannerRouter.get('/getall', authenticateToken, getAllAppBanners);
+appBannerRouter.get('/getall', adminAuthenticateToken, getAllAppBanners);
 
 // Get a specific app banner by ID
-appBannerRouter.get('/:id', authenticateToken, getAppBannerById);
+appBannerRouter.get('/:id', adminAuthenticateToken, getAppBannerById);
 
 // Delete an app banner by ID
-appBannerRouter.delete('/:id', authenticateToken, deleteAppBannerById);
+appBannerRouter.delete('/:id', adminAuthenticateToken, deleteAppBannerById);
 
 export default appBannerRouter;
