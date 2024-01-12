6fa1287ecef7762000ae52e8fb0e660aec2c49d4
Mohd Mujahed <mujahedu984@gmail.com>
2024-01-12 11:38:16 +0530

    update  : api

--- frontend/axiosUtility/api.js
+++ frontend/axiosUtility/api.js
@@ -1,10 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:4040',  
+  baseURL: 'http://localhost:4040',
 });
-
- 
 
 export const fetchData = async (endpoint, config = {}) => {
   try {
@@ -16,6 +14,16 @@ export const fetchData = async (endpoint, config = {}) => {
     return response.data;
   } catch (error) {
     console.error('Axios request error:', error);
+    throw error;
+  }
+};
+
+export const postData = async (endpoint, data, config = {}) => {
+  try {
+    const response = await instance.post(endpoint, data, config);
+    return response.data;
+  } catch (error) {
+    console.error('Axios POST request error:', error);
     throw error;
   }
 };
