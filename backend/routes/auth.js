cebae541e29f8b18b9fb3b209a22ef5e35f468fa
Mohammed Arshad <mohammedarshad.arsh@gmail.com>
2024-01-21 05:04:16 +0530

    Add delete functionality and getUserById API endpoint

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { signin, sendOTPforverification, verifyotp, register, getAllCustomers, getAlldeliveryagent, getallTeamMembers ,} from '../controllers/auth.js';
+import { signin, sendOTPforverification, verifyotp, register, getAllCustomers, getAlldeliveryagent, deletebyid, getUserById ,} from '../controllers/auth.js';
 import { authenticateToken } from '../middleware/authToken.js';
 const router = express.Router();
 router.post("/register",register)
@@ -8,13 +8,9 @@ router.post("/emailOtpSend", sendOTPforverification );
 router.post("/emailOtpVerify", verifyotp );
 router.get("/getallcustomers",getAllCustomers)
 router.get("/getalldeliveryagent",getAlldeliveryagent)
-router.get("/getallTeamMembers",getallTeamMembers)
-
-
-
-
+// router.get("/getallTeamMembers",getallTeamMembers)
+router.delete("/id/:id",deletebyid)
+router.get("/id/:id",getUserById)
+// router.post("/facebook",facebookAuth)
 // router.post("/google",googleAuth)
-
-
-
 export default router
