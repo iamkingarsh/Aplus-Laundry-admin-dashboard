34e48ea6b6a3b2d675db32374db0a74f25358b12
Mohd Mujahed <mujahedu984@gmail.com>
2024-01-22 14:00:52 +0530

    updated mobile otp   : auth

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,11 +1,16 @@
 import express from 'express';
-import { signin, sendOTPforverification, verifyotp, register, getAllCustomers, getAlldeliveryagent, getallTeamMembers ,} from '../controllers/auth.js';
-import { authenticateToken } from '../middleware/authToken.js';
+import { signin, sendOTPforverification, verifyotp, register, getAllCustomers, getAlldeliveryagent, getallTeamMembers, sendOTPforMobileverification, verifymobileotp ,} from '../controllers/auth.js';
+import { adminAuthenticateToken, authenticateToken } from '../middleware/authToken.js';
 const router = express.Router();
 router.post("/register",register)
-router.post("/signin",signin)
+router.post("/signin",signin)     
 router.post("/emailOtpSend", sendOTPforverification );
+router.post("/mobileOtpSend", sendOTPforMobileverification );
+
+router.post("/adminEmailOtpSend",adminAuthenticateToken, sendOTPforverification );
 router.post("/emailOtpVerify", verifyotp );
+router.post("/mobileOtpVerify", verifymobileotp );
+
 router.get("/getallcustomers",getAllCustomers)
 router.get("/getalldeliveryagent",getAlldeliveryagent)
 router.get("/getallTeamMembers",getallTeamMembers)
