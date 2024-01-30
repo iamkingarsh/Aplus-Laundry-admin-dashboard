c5d4aa88008db2fd062eb908f37076a4e7ed9c6c
Mohammed Arshad <mohammedarshad.arsh@gmail.com>
2024-01-30 13:28:58 +0530

    test subscription routes

--- backend/routes/order.js
+++ backend/routes/order.js
@@ -1,18 +1,21 @@
 import express from 'express';
- 
- 
-import { authenticateToken } from '../middleware/authToken.js';
-import { createOrUpdateOrder, deleteOrderById, getAllOrders, getOrderById, savePayment, updateOrderStatusById }  from '../controllers/order.js';
+
+
+import { adminAuthenticateToken, authenticateToken } from '../middleware/authToken.js';
+import { createOrUpdateOrder, createPlan, deleteOrderById, getAllOrders, getOrderById, savePayment, updateOrderStatusById, subscribeToPlans } from '../controllers/order.js';
 
 
 const orderRouter = express.Router();
 
-orderRouter.post('/addorupdate', authenticateToken, createOrUpdateOrder);  
-orderRouter.post('/save', authenticateToken, savePayment);  
+orderRouter.post('/addorupdate', authenticateToken, createOrUpdateOrder);
+orderRouter.post('/save', authenticateToken, savePayment);
 
 orderRouter.get('/getall', authenticateToken, getAllOrders);
 orderRouter.get('/:id', authenticateToken, getOrderById);
 orderRouter.put('/:id/status', authenticateToken, updateOrderStatusById);
-orderRouter.delete('/:id', authenticateToken, deleteOrderById); 
+orderRouter.delete('/:id', authenticateToken, deleteOrderById);
+
+orderRouter.post('/createPlan', authenticateToken, createPlan);
+orderRouter.post('/subscribeToPlans', authenticateToken, subscribeToPlans);
 
 export default orderRouter;
