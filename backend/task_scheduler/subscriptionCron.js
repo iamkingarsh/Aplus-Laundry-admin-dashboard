2bbba8411874214610b3c3f6f68f19c4324b8d88
Mohd Mujahed <mujahedu984@gmail.com>
2024-02-11 04:21:48 +0530

    updated cron jobs

---
+++ backend/task_scheduler/subscriptionCron.js
@@ -0,0 +1,26 @@
+// subscriptionCron.js
+
+import cron from 'node-cron';
+import User from "../models/user.js";
+
+const checkSubscription = cron.schedule('* * * * * *', async () => { // Update the schedule to run every second
+    console.log('Customer types update started.');
+
+    try {
+        const usersToUpdate = await User.find({
+            customerType: 'subscriber',
+            subscriptionEndDate: { $lte: new Date() } 
+        });
+
+        await Promise.all(usersToUpdate.map(async (user) => {
+            user.customerType = 'nonsubscriber';
+            await user.save();
+        }));
+
+        console.log('Customer types updated successfully.');
+    } catch (error) {
+        console.error('Error updating customer types:', error);
+    }
+});
+
+export default checkSubscription;
