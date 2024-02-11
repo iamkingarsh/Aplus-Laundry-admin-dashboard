5ca3490b0b0b17e61b9708582fc3239238f402c1
Mohd Mujahed <mujahedu984@gmail.com>
2024-02-11 12:42:12 +0530

    updated   : razorpaysubcriptions

--- backend/routes/razorpaysubcriptions.js
+++ backend/routes/razorpaysubcriptions.js
@@ -5,7 +5,7 @@ import express from 'express';
 import { adminAuthenticateToken, authenticateToken } from '../middleware/authToken.js';
 import { deleteTransactionById, getAllTransactions, getTransactionsById } from '../controllers/transaction.js';
 
-import { createPlan, createSubscriptionCheckout, deletePlan, getAllPlans } from '../controllers/razorpaySubscriptions.js';
+import { createPlan, createSubscriptionCheckout, deletePlan, getAllPlans, saveSubscriptionPayment } from '../controllers/razorpaySubscriptions.js';
 
 
 const razorpaySubcriptionRouter = express.Router();
@@ -18,6 +18,8 @@ razorpaySubcriptionRouter.post('/createNewPlan', authenticateToken, createPlan);
 razorpaySubcriptionRouter.get('/getallPlans', authenticateToken, getAllPlans);
 razorpaySubcriptionRouter.get('/getPlanById/:id', authenticateToken, getTransactionsById);
 razorpaySubcriptionRouter.post('/createSubscriptionCheckout', authenticateToken, createSubscriptionCheckout);
+razorpaySubcriptionRouter.post('/subscriptionTransaction/save', authenticateToken, saveSubscriptionPayment);
+
 razorpaySubcriptionRouter.delete('/delete/:id', adminAuthenticateToken, deleteTransactionById);
 razorpaySubcriptionRouter.delete('/deletePlan/:id', adminAuthenticateToken, deletePlan);
 
