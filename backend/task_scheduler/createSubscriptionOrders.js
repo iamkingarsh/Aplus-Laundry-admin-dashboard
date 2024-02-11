cfeed4052255b304c9542a75484c2aa39423bc6b
Mohd Mujahed <mujahedu984@gmail.com>
2024-02-12 00:20:23 +0530

    updated   : createSubscriptionOrders

--- backend/task_scheduler/createSubscriptionOrders.js
+++ backend/task_scheduler/createSubscriptionOrders.js
@@ -19,41 +19,48 @@ const createSubscriptionOrders = async () => {
         pickupTime: '10:00 AM to 2:00 PM'
       };
 
-      if (new Date().getDay() === 2) {
-        pickupDetails.pickupDate.setDate(pickupDetails.pickupDate.getDate() + 1); // Wednesday
-      } else if (new Date().getDay() === 6) {
-        pickupDetails.pickupDate.setDate(pickupDetails.pickupDate.getDate() + 2); // Sunday
-      } else {
-        return; // Skip if not Tuesday or Saturday
+      const today = new Date().getDay();
+      if (today === 2 || today === 6) { // Tuesday (2) or Saturday (6)
+        if (today === 2) {
+          pickupDetails.pickupDate.setDate(pickupDetails.pickupDate.getDate() + 1); // Wednesday
+        } else if (today === 6) {
+          pickupDetails.pickupDate.setDate(pickupDetails.pickupDate.getDate() + 2); // Sunday
+        }
+
+        const subscriptionOrder = new SubscriptionOrder({
+          order_id: await generateOrderId(), // Implement this function to generate unique order ID
+          order_type: 'subscription',
+          service: service._id,
+          customer: user._id,
+          status: 'pending',
+          pickupDetails: pickupDetails,
+          subscription_id: subscription._id
+        });
+
+        await subscriptionOrder.save();
       }
-
-      const subscriptionOrder = new SubscriptionOrder({
-        order_id: generateOrderId(), // Implement this function to generate unique order ID
-        order_type: 'subscription',
-        service: service._id,
-        customer: user._id,
-        status: 'pending',
-        pickupDetails: pickupDetails,
-        subscription_id: subscription._id
-      });
-
-      await subscriptionOrder.save();
     });
 
-    console.log('Subscription orders created successfully',users);
+    console.log('Subscription orders created successfully', users);
   } catch (error) {
     console.error('Error creating subscription orders:', error);
   }
 };
 
-const createSubscriptionOrdersCron = cron.schedule('* * * * * *', () => {
-  console.log('Running cron job every 5 minutes...');
+const createSubscriptionOrdersCron = cron.schedule('0 23 * * 2,6', () => {
+  console.log('Running cron job every Tuesday and Saturday at 11 PM...');
   createSubscriptionOrders();
 });
 
-const generateOrderId = () => {
-
-
+const generateOrderId = async () => {
+  try {
+    // Get the number of documents in the SubscriptionOrder collection
+    const orderCount = await SubscriptionOrder.countDocuments();
+    return `All_SUB_${orderCount}`;
+  } catch (error) {
+    console.error('Error counting SubscriptionOrder documents:', error);
+    return null;
+  }
 };
 
 export default createSubscriptionOrdersCron;
