42f5796876bcf2eec817984ee5213224880e7cb2
Mohd Mujahed <mujahedu984@gmail.com>
2024-02-12 10:41:20 +0530

    update

--- backend/task_scheduler/subscriptionCron.js
+++ backend/task_scheduler/subscriptionCron.js
@@ -1,25 +1,48 @@
-// subscriptionCron.js
-
 import cron from 'node-cron';
-import User from "../models/user.js";
+import User from '../models/user.js';
+import SubscriptionTransaction from '../models/subscriptionTransaction.js';
+import Razorpay from 'razorpay';
 
-const checkSubscription = cron.schedule('* * * * * *', async () => { // Update the schedule to run every second
+const razorpay = new Razorpay({
+    key_id: process.env.RAZORPAY_KEY_ID,
+    key_secret: process.env.RAZORPAY_SECRET,
+});
 
-    try {
-        const usersToUpdate = await User.find({
-            customerType: 'subscriber',
-            subscriptionEndDate: { $lte: new Date() } 
-        });
+const checkSubscription = cron.schedule('0 0 * * *', async () => {
+  try {
+    const users = await User.find({ customerType: 'subscriber' });
+    console.log('users',users)
 
-        await Promise.all(usersToUpdate.map(async (user) => {
-            user.customerType = 'nonsubscriber';
-            await user.save();
-        }));
+    // Iterate over each user
+    for (const user of users) {
+      let allInactive = true;  
 
-        // console.log('Customer types updated successfully.');
-    } catch (error) {
-        console.error('Error updating customer types:', error);
+      // Find all subscription transactions for the current user
+      const subscriptionTransactions = await SubscriptionTransaction.find({ customer_id: user._id });
+
+      console.log('transactiontransaction',subscriptionTransactions)
+      // Iterate over each subscription transaction
+      for (const transaction of subscriptionTransactions) {
+          // Fetch subscription details from Razorpay
+          const fetchedSubscription = await razorpay.subscriptions.fetch(transaction.razorpay_subscription_id);
+
+        if (fetchedSubscription.status === 'active') {
+          allInactive = false; 
+          break;  
+        }
+      }
+
+      // If all subscriptions are inactive, update user's customerType and remove subscription_id
+      if (allInactive) {
+        await User.findByIdAndUpdate(user._id, { $set: { customerType: 'nonsubscriber' }, $unset: { subscription_id: '' } });
+        console.log(`User ${user._id}: All subscriptions are inactive. CustomerType updated to 'nonsubscriber'.`);
+      }
     }
+
+    console.log('Subscription check completed.');
+  } catch (error) {
+    console.error('Error checking subscriptions:', error);
+  }
 });
 
 export default checkSubscription;
