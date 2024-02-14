91b25e649ce74fbee19e9ba4bae3695858bc05b3
Mohammed Arshad <mohammedarshad.arsh@gmail.com>
2024-02-15 01:47:20 +0530

    updated order

--- backend/routes/order.js
+++ backend/routes/order.js
@@ -2,7 +2,7 @@ import express from 'express';
 
 
 import { adminAuthenticateToken, authenticateToken } from '../middleware/authToken.js';
-import { createOrUpdateOrder, deleteOrderById, getAllOrders, getOrderById, savePayment, updateOrderStatusById } from '../controllers/order.js';
+import { createOrUpdateOrder, deleteOrderById, getAllOrders, getOrderById, getUserOrders, savePayment, updateOrderStatusById } from '../controllers/order.js';
 
 
 const orderRouter = express.Router();
@@ -14,6 +14,7 @@ orderRouter.get('/getall', authenticateToken, getAllOrders);
 orderRouter.get('/:id', authenticateToken, getOrderById);
 orderRouter.put('/:id/status', adminAuthenticateToken, updateOrderStatusById);
 orderRouter.delete('/:id', adminAuthenticateToken, deleteOrderById);
+orderRouter.get('/getuserorders/:userId', authenticateToken, getUserOrders);
 
 
 
