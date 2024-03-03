8e2a790de47590e758dbe26289c5adb1c60faeb4
Mohd Mujahed <mujahedu984@gmail.com>
2024-03-04 04:03:06 +0530

    updated   : authToken

--- backend/middleware/authToken.js
+++ backend/middleware/authToken.js
@@ -36,32 +36,30 @@ export const authenticateToken = (req, res, next) => {
 
 
 export const adminAuthenticateToken = (req, res, next) => {
-  console.log('authenticateToken', req.headers.authorization);
-
-  const authorizationHeader = req.headers.authorization;
-
-  if (!authorizationHeader || !authorizationHeader.startsWith('Bearer ')) {
-    return res.status(401).json({ message: 'Unauthorized' });
-  }
-
-  const token = authorizationHeader.split('Bearer ')[1];
-
-  if (!token) {
-    return res.status(401).json({ message: 'Unauthorized' });
-  }
-
   try {
+    const authorizationHeader = req.headers.authorization;
+
+    if (!authorizationHeader || !authorizationHeader.startsWith('Bearer ')) {
+      return res.status(401).json({ message: 'Authorization header missing or invalid' });
+    }
+
+    const token = authorizationHeader.split('Bearer ')[1];
+
+    if (!token) {
+      return res.status(401).json({ message: 'Token not found in Authorization header' });
+    }
+
     const decoded = jwt.verify(token, process.env.JWT_SECRETKEY);
-    console.log('decoded decoded' , decoded)
-    // Check if the required role is present in the decoded token
+    console.log('Decoded token:', decoded);
+
     if (!decoded.role || !['owner', 'admin'].includes(decoded.role)) {
-      return res.status(403).json({ message: 'Forbidden' });
+      return res.status(403).json({ message: 'Forbidden: Insufficient permissions' });
     }
 
     req.user = decoded;
     next();
   } catch (err) {
     console.error('Token verification error:', err.message);
-    return res.status(403).json({ message: 'Forbidden' });
+    return res.status(403).json({ message: 'Forbidden: Token verification failed' });
   }
 };
