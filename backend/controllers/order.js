25ae26d7f58dbfdc87b605001c9b1b5d7e455961
Mohd Mujahed <mujahedu984@gmail.com>
2024-03-11 14:20:01 +0530

    updated   : order

--- backend/controllers/order.js
+++ backend/controllers/order.js
@@ -137,6 +137,7 @@ export const createOrUpdateOrder = async (req, res) => {
 
 const sendOrderConfirmationEmail = async (updatedOrder_id,cartTotal,status) => {
     try {
+        console.log('Sending order confirmation email')
         // Fetch the order details from the database and populate related fields
         const order = await Order.findById(updatedOrder_id)
             .populate({
@@ -319,6 +320,7 @@ export const getAllOrders = async (req, res) => {
             .populate('service', 'serviceTitle')
             .populate('products.id', 'product_name')
             .populate('customer', 'fullName mobileNumber')
+            .populate('transaction_id')
             .populate('coupon_id', null, { _id: { $exists: true } });
 
         // Check if the address query parameter is provided
